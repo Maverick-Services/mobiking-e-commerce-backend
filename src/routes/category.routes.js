47bd5dda56de909227453555498042bf8da48d4f
Maverick-Services <maverickit123@gmail.com>
2025-06-03 16:11:17 +0530

    Category route updated

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -23,7 +23,7 @@ router.route("/createCategory").post(verifyJWT,
 router.route("/:_id").put(verifyJWT, editCategory);
 router.route("/:_id").delete(verifyJWT, deleteCategory);
 router.route("/").get(getAllCategories);
-router.route("/details/:_id").get(verifyJWT, getCategoryById);
+router.route("/view/:_id").get(verifyJWT, getCategoryById);
 router.route("/details/:slug").get(getCategoryBySlug);
 
 //Sub Category Routes
@@ -47,7 +47,7 @@ router.route("/subCategories/:_id").put(verifyJWT, editSubCategory);
 router.route("/subCategories/:_id").delete(verifyJWT, deleteSubCategory);
 router.route("/subCategories").get(getAllSubCategories);
 router.route("/subCategories/featured").get(getAllFeaturedSubCategories);
-router.route("/subCategories/details/:_id").get(verifyJWT, getSubCategoryById);
+router.route("/subCategories/view/:_id").get(verifyJWT, getSubCategoryById);
 router.route("/subCategories/details/:slug").get(getSubCategoryBySlug);
 
 export default router
