735129a784fedd8472f52a277f81204bb9d02e07
Maverick-Services <maverickit123@gmail.com>
2025-07-01 13:00:33 +0530

    Updated Product model

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -34,7 +34,7 @@ const productSchema = new mongoose.Schema({
         required: true,
     },
     descriptionPoints: {
-        type: String,
+        type: [String],
     },
     keyInformation: {
         type: mongoose.Schema.Types.Mixed,
